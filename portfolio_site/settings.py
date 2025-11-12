a0c6489dedd05dedd91c0e3c130e8ff50d63128c
ronnkarani <karanironny25@gmail.com>
2025-11-12 13:48:33 +0300

    render settings

--- portfolio_site/settings.py
+++ portfolio_site/settings.py
@@ -2,10 +2,8 @@ import os
 from pathlib import Path
 from decouple import config
 
-
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
-
 
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/5.2/howto/deployment/checklist/
@@ -16,8 +14,12 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = config('DEBUG', default=False, cast=bool)
 
-ALLOWED_HOSTS = ['*']
-
+ALLOWED_HOSTS = [
+    '*',
+    'ronnkarani.onrender.com',  
+    'localhost',
+    '127.0.0.1'
+]
 
 # Application definition
 
@@ -111,8 +113,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.2/howto/static-files/
 
 STATIC_URL = 'static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]  # or remove if unused
-STATIC_ROOT = BASE_DIR / 'staticfiles'    # where collectstatic dumps to
+STATICFILES_DIRS = [BASE_DIR / "static"]  
+STATIC_ROOT = BASE_DIR / 'staticfiles'    
 STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
 
 # Default primary key field type
@@ -156,6 +158,9 @@ DEFAULT_FROM_EMAIL = 'karanironny25@gmail.com'
 
 # HTTPS & cookies
 # settings.py
+CSRF_TRUSTED_ORIGINS = [
+    'https://ronnkarani.onrender.com',
+]
 
 
 if not DEBUG:
